85d2c4704f58c3cc024b278fc0f1429e2f764c49
Da Min Joo <damin.joo0725@gmail.com>
2022-11-25 18:34:10 -0500

    Update TicTacToeGame.py

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -28,7 +28,6 @@ while(True):
     while(True):
         userinputR = input("Enter the row from 0, 1, 2: ")
         userinputC = input("Enter the column from 0, 1, 2: ")
-        print(" ")
         
         if "2" or "1" or "0" in (userinputR, userinputC):
             if " O" in game[int(userinputR)][int(userinputC)]:
@@ -44,6 +43,15 @@ while(True):
     printGame()  
         
     compRandom()
+    
+    game=[[" X"," 2"," X"],[" X","22"," O"],[" 1","01","20"]]
+    
+    #game stops when a column is all X or all O
+    
+    #game stops when a row is all X or all O
+    
+    #game stops when diagonal is all X or all O
+
     
     #game stops when all inputs are X or O
     if all(all(item == " O" or item == " X" for item in items) for items in game):
