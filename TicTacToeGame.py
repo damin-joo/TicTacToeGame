e658f0f9352d5b40bf6c653576d4750fff33569f
Da Min Joo <damin.joo0725@gmail.com>
2022-11-25 18:12:08 -0500

    Update TicTacToeGame.py

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -1,36 +1,53 @@
 import random
+from itertools import chain
 
 print("Tic Tac Toe Game")
-
-print("In the two digit below, the first digit represents row, and second digit represents column")
+print("First digit represents row, and second digit represents column")
 game=[["00","01","02"],["10","11","12"],["20","21","22"]]
 
-while(True):
-    #use for loop to iterate the array
+def printGame():
     for rows in game:
         for columns in rows:
            print(columns,end=" ")
         print()
+    print(" ")
+
+def compRandom():
+    compChoiceR = random.randint(0,2)
+    compChoiceC = random.randint(0,2)
+    game[compChoiceR][compChoiceC] = " O"
+
+
+printGame()
+compRandom()
+
+while(True):
+    #use for loop to iterate the array
+    printGame()
     
     while(True):
         userinputR = input("Enter the row from 0, 1, 2: ")
         userinputC = input("Enter the column from 0, 1, 2: ")
+        print(" ")
         
-        if ("2" or "1" or "0") in (userinputR, userinputC):
-            game[int(userinputR)][int(userinputC)] = "X"
-            break
+        if "2" or "1" or "0" in (userinputR, userinputC):
+            if " O" in game[int(userinputR)][int(userinputC)]:
+                print("Value already exist in the space, input a different value")
+            elif " X" in game[int(userinputR)][int(userinputC)]:
+                print("Value already exist in the space, input a different value")
+            else:
+                game[int(userinputR)][int(userinputC)] = " X"
+                break
         else:
             print("Invalid input")
     
-    compChoiceR = random.randint(0,2)
-    compChoiceC = random.randint(0,2)
-    game[compChoiceR][compChoiceC] = "O"
+    printGame()  
+        
+    compRandom()
     
-    #game runs until all inputs are "X" or "O"
-    for rows in game:
-        if ("X" or "O") in rows
-        for columns in rows:
-            if ("X" or "O") in columns
-            print(columns,end=" ")
-            break
-        print()
+    #game stops when all inputs are X or O
+    if all(all(item == " O" or item == " X" for item in items) for items in game):
+        printGame()
+        break
+
+        
